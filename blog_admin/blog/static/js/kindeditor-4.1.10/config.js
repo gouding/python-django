985c43061a97eb9327e355ec42ec9ab2c0d872bb
denzel <422293027@qq.com>
2017-03-13 17:16:50 +0800

    devleop

--- blog_admin/blog/static/js/kindeditor-4.1.10/config.js
+++ blog_admin/blog/static/js/kindeditor-4.1.10/config.js
@@ -1,7 +1,7 @@
 KindEditor.ready(function(K) {
     window.editor = K.create('#id_content',{
         // 指定大小
-        width:'700px',
-        height:'200px',
+        width:'800px',
+        height:'450px',
     });
 });
